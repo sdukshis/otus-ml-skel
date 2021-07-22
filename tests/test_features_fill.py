db9bb103b70bba6efaa843a61634baef75a32db3
Igor Garaev <tigersp@mail.ru>
2021-07-22 19:41:14 +0300

    #1 tester

---
+++ tests/test_features_fill.py
@@ -0,0 +1,16 @@
+import pytest
+
+import pandas as pd
+
+from titanic.features.fill import *
+
+
+@pytest.mark.parametrize("test_input,expected", [((1,2,4,None,4,4,4,5), (1,2,4,4,4,4,4,5) )])
+def test_fill_age(test_input, expected):
+    df = pd.DataFrame(
+        data={"SibSp": [test_input[0]], "Parch": [test_input[1]]},
+    )
+    assert expected == family_size(df).iloc[0]
+
+
+
