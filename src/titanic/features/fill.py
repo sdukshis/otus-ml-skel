8982c5fc1974e4e15b29d9e07b7bebfce0e10881
Bulat <bula-bula2@yandex.ru>
2023-06-17 02:01:21 +0300

    Fix and add chages in fill_age function

--- src/titanic/features/fill.py
+++ src/titanic/features/fill.py
@@ -2,7 +2,7 @@ import pandas
 from sklearn.impute import SimpleImputer
 
 
-__all__ = ["embarked_imputer", "fill_embarked"]
+__all__ = ["embarked_imputer", "fill_embarked", "fill_age"]
 
 
 def embarked_imputer() -> SimpleImputer:
@@ -14,15 +14,31 @@ def fill_embarked(df: pandas.DataFrame) -> pandas.Series:
 
 
 def fill_age(df: pandas.DataFrame, inplace=False) -> pandas.DataFrame:
+
+    if df.shape[0] == 0:
+        raise ValueError('df is empty')
+
+    if df[df['Age'].notna()].shape[0] == 0:
+        raise ValueError('The "Age" column is fully missed')
+
     if not inplace:
         transformed = df.copy(deep=True)
     else:
         transformed = df
 
-    dict_to_age_map = df.groupby('Pclass')['Age'].median().astype(int).to_dict()
-    for pclass in df['Pclass'].unique():
+    age_all_over_cls = int(df['Age'].median())
+    age_grouped = df.groupby('Pclass')['Age'].median()
+    age_grouped.loc[age_grouped.isna()] = age_all_over_cls
+
+    # Словарь, в котором не учтены NaN Pclass
+    dict_to_age_map = age_grouped.astype(int).to_dict()
+
+    for pclass, age_median in dict_to_age_map.items():
         nan_mask = (df['Pclass'] == pclass) & df['Age'].isna()
-        transformed.loc[nan_mask, 'Age'] = dict_to_age_map[pclass]
+        transformed.loc[nan_mask, 'Age'] = age_median
+
+    # Заполнение тех, у которых Pclass был NaN и их не заполнили в предыдущих шагах
+    transformed['Age'] = transformed['Age'].fillna(age_all_over_cls)
 
     return transformed
 
@@ -31,6 +47,8 @@ if __name__ == '__main__':
     from titanic.data.make_dataset import load_titanic
 
     titanic = load_titanic()
+    import numpy as np
+    titanic.loc[1, ['Pclass', 'Age']] = np.nan
     titanic = fill_age(titanic, inplace=False)
 
     assert titanic[titanic['Age'].isna()].shape[0] == 0
